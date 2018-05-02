90db1151c7b461de1adec0768e164f8a88595586
hugovk <hugovk@users.noreply.github.com>
2018-05-02 20:55:55 +0300

    Add scraper

--- scraper.py
+++ scraper.py
@@ -1,24 +1,54 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+from requests_html import HTMLSession  # pip install requests-html
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
+import scraperwiki
+
+MPS_A_Z_URL = "https://www.eduskunta.fi/FI/kansanedustajat/Sivut/Kansanedustajat-aakkosjarjestyksessa.aspx"
+
+
+def scrape_list(url):
+    """Parse the list of politicians, isolating each one by name"""
+
+    r = session.get(url)
+    mps_list = r.html.find("#MSOZoneCell_WebPartWPQ3", first=True)
+    mps = mps_list.find(".dfwp-item")
+
+    for mp in mps:
+        person = {}
+        person["name"] = mp.find("a", first=True).text
+        person["party"] = mp.find(".description", first=True).text
+        mp_url = mp.links.pop()
+
+        print("{}\t{}\t{}".format(mp_url, person["party"], person["name"]))
+
+        scrape_person(person, mp_url)
+
+
+def scrape_person(person, url):
+    """Parse the data for each individual politician"""
+
+    r = session.get(url)
+    table = r.html.find(".mopPersonTable", first=True)
+    rows = table.find("tr")
+
+    for row in rows:
+        cells = row.find("td")
+        key = cells[0].text.rstrip(":")
+        person[key] = cells[1].text
+
+        print("{}:\t{}".format(key, person[key]))
+
+        # Write out to the sqlite database using scraperwiki library
+        scraperwiki.sqlite.save(unique_keys=["name"], data=person)
+
+
+session = HTMLSession()
+
+scrape_list(MPS_A_Z_URL)
+
+
 # # An arbitrary query against the database
 # scraperwiki.sql.select("* from data where 'name'='peter'")
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
 # All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+# called "data.sqlite" in the current working directory which has at least a
+# table called "data".
